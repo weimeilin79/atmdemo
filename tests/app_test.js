f3f73b2829ae0ff4a4d00492745af2f0ab444ae4
weimeilin79 <weimeilin@gmail.com>
2018-01-10 13:13:59 -0500

    work

--- tests/app_test.js
+++ tests/app_test.js
@@ -9,23 +9,14 @@ reqServer = process.env.HTTP_TEST_SERVER || server
 
 describe('Basic routes tests', function() {
 
-    it('GET to / should return 200', function(done){
+    
+    it('GET to /atms should return 200', function(done){
         chai.request(reqServer)
-        .get('/')
+        .get('/atms')
         .end(function(err, res) {
             res.should.have.status(200);
-            done();
+           
         })
-
-    })
-
-    it('GET to /pagecount should return 200', function(done){
-        chai.request(reqServer)
-        .get('/pagecount')
-        .end(function(err, res) {
-            res.should.have.status(200);
-            done();
-        })
-
+				done();
     })
 })
