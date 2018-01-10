6a12c8e16d91d4a0f7bfad755a9a2b82a5d92ed8
weimeilin79 <weimeilin@gmail.com>
2018-01-10 11:59:40 -0500

    work

--- tests/app_test.js
+++ tests/app_test.js
@@ -15,8 +15,8 @@ describe('Basic routes tests', function() {
         .get('/atms')
         .end(function(err, res) {
             res.should.have.status(200);
-            done();
+           
         })
-
+				 done();
     })
 })
